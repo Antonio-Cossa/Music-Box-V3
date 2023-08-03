25d607b56d19a192f51d518553773d97d8d261ed
Antonio <antoniovcossa16@gmail.com>
2023-08-03 22:32:48 +0200

    correcting usercSchema username unique

--- Backend/API/src/models/User.js
+++ Backend/API/src/models/User.js
@@ -5,11 +5,12 @@ const UserSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
-        unique: true,
+
     },
     username: {
         type: String,
         required: true,
+        unique: true
     },
     password: {
         type: String,
